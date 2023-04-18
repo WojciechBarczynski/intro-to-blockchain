6fe755bc4211aac9c12bff70bffb68d9818555aa
Wojciech Barczynski <barczynski.w@gmail.com>
2023-04-18 16:38:32 +0200

    Add keys encryption / decryption

--- exercise1/public_key_2.py
+++ exercise1/public_key_2.py
@@ -13,33 +13,15 @@ class Alice:
         self.bob = bob
 
     def encrypt(self, message: str) -> bytes:
-        """
-        TODO: zaimplementuj szyfrowanie wiadomości
-            Przydadzą ci się funkcje/metody
-                - self.bob.get_public_key()
-                - bytes(string, 'utf-8')
-                - asymmetric_encrypt
-        """
-        raise NotImplementedError()
+        return asymmetric_encrypt(self.bob.get_public_key(), bytes(message, 'utf-8'))
 
 
 class Bob:
     def __init__(self):
-        """
-        TODO: wygeneruj parę klucz publiczny, prywatny za pomocą metody generate_key_pair z simple_cryptography.
-        """
-        # self._public_key, self._private_key = ?
-        raise NotImplementedError()
+        self._public_key, self._private_key = generate_key_pair()
 
     def get_public_key(self):
         return self._public_key
 
     def decrypt(self, encrypted_message: bytes) -> str:
-        """
-        TODO: Zaimplementuj deszyfrowanie wiadomości.
-            Przydadzą ci się funkcje/metody:
-                - self._private_key
-                - asymmetric_decrypt
-                - string.decode("utf-8")
-        """
-        raise NotImplementedError()
+        return asymmetric_decrypt(self._private_key, encrypted_message).decode('utf-8')
